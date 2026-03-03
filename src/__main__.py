329b0e1c99fcf705ea92e4951b18aff4f53c0e6d
Christian Schinkel <107631710+ChristianSchinkel@users.noreply.github.com>
2026-03-03 23:31:41 +0100

    add application initialization and requirements installation

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,10 @@
 with HL7 FHIR integration."""
 
 
+import subprocess
+import sys
+
+
 def main():
     """The main function for the Health Informatics Lab application."""
     initialize_application()
@@ -10,8 +14,26 @@ def main():
 def initialize_application():
     """Initialize the Health Informatics Lab application."""
     print("Welcome to the Health Informatics Lab with HL7 FHIR integration!")
+    install_requirements()
     # This function can be used to initialize the application
     # and set up any necessary configurations.
+
+
+# If reqired libraries or modules need to be installedd, they can be installed.
+def install_requirements():
+    """Install any required dependencies for the application."""
+    # Check "requirements.txt" for any required libraries and install them.
+    # write code to install dependencies if needed, e.g.,
+    # using subprocess to call pip.
+    # Example:
+    # import subprocess
+    if sys.version_info < (3, 7):
+        print("This application requires Python 3.7 or higher.")
+        sys.exit(1)
+    subprocess.check_call(
+        [sys.executable,
+         "-m", "pip", "install", "-r", "./requirements/requirements.txt"]
+        )
 
 
 if __name__ == "__main__":
